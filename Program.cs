8427e019978301b94f2d3f418b4626dffdf0decd
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-23 20:11:44 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 5;
+            String name = "Pomni";
+            Char chr = '#';
+
+            String username = "Uzi";
+            int age = 18;
+            const double pi = 3.1415926f;
+
+            Console.WriteLine(x + y);
+            Console.WriteLine(name + chr);
+            Console.WriteLine("Im " + username + " and im " + age + " years old");
+            Console.WriteLine("Pi = " + pi);
+        }
+    }
+}
