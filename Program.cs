76474141d13923e1d769f0eb9bcba8cf0ac6ff91
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-25 17:32:47 +0800

    Minor change

--- Program.cs
+++ Program.cs
@@ -13,11 +13,9 @@ namespace csharp
                 int y = Convert.ToInt32(Math.Pow(x, i));
                 Console.WriteLine(y);
             }
-            for (int i = 0; i < 10; i++)
-            {
-                double z = Math.Sqrt(x);
-                Console.WriteLine(z);
-            }
+
+            double z = Math.Sqrt(x);
+            Console.WriteLine(z);
         }
     }
 }
